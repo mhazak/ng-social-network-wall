435e2ca5a983c7ddcb936948b9c72a24863d4555
mhazak <hazak.marek@gmail.com>
2022-07-02 10:07:02 +0200

    [dev] snackbar with err messages to login

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { UiService } from './ui/ui.service';
 
 import { User } from './user.model';
 
@@ -9,7 +10,7 @@ import { User } from './user.model';
 })
 export class UserService {
 
-	constructor(private http: HttpClient) { }
+	constructor(private http: HttpClient, private uiService: UiService) { }
 
 	createUser(user: User) {
 		this.http.post(environment.backend + '/users', user).subscribe(res => {
@@ -23,6 +24,7 @@ export class UserService {
 		this.http.get<User[]>(environment.backend + '/users?email=' + user.email).subscribe(res => {
 			console.log({res});
 			if (!res.length) {
+				this.uiService.snackbarOpen('User was not founded! Please check your e-mail address.', null, { duration: 3000 })
 				console.log('no users founded!');
 				return;
 			}
@@ -30,6 +32,7 @@ export class UserService {
 			if (_user.password === user.password) {
 				console.log('successs');
 			} else {
+				this.uiService.snackbarOpen('Login failed. Please check your e-mail address and password.', null, { duration: 3000 })
 				console.log('invalid!');
 			}
 		}, err => {
