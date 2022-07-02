03b6f67238eafb30284ab76ee7780e39746225d7
mhazak <hazak.marek@gmail.com>
2022-07-02 09:01:13 +0200

    [dev] styling and adding reactive module

--- src/app/pages/create-account/create-account.component.ts
+++ src/app/pages/create-account/create-account.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-account',
   templateUrl: './create-account.component.html',
   styleUrls: ['./create-account.component.css']
 })
+
 export class CreateAccountComponent implements OnInit {
 
-  constructor() { }
+	hidePassword: boolean = true;
+	signupForm = new FormGroup({
+		email: new FormControl('', [Validators.email, Validators.required]),
+		password: new FormControl('', [Validators.required, Validators.minLength(6)])
+	})
+  	constructor() { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
+
+	onSubmit() {
+
+	}
 
 }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,14 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+	hidePassword: boolean = true;
+	loginForm = new FormGroup({
+		email: new FormControl('', [Validators.required, Validators.email]),
+		password: new FormControl('', [Validators.required])
+	});
 
-  ngOnInit(): void {
-  }
+  	constructor() { }
+
+	ngOnInit(): void {
+
+	}
+
+	onSubmit() {
+		console.log('submitted!');
+	}
 
 }
