2f09753a7fd724fee86fd62d36409c30ef1dd3f7
mhazak <hazak.marek@gmail.com>
2022-07-02 10:03:07 +0200

    [init] snackbar

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -4,19 +4,22 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
 	imports: [
 		MatButtonModule,
 		MatFormFieldModule,
 		MatInputModule,
-		MatIconModule
+		MatIconModule,
+		MatSnackBarModule
 	],
 	exports: [
 		MatButtonModule,
 		MatFormFieldModule,
 		MatInputModule,
-		MatIconModule
+		MatIconModule,
+		MatSnackBarModule
 	]
 })
 
